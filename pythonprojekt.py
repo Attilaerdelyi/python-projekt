556d4e2a394ffdfd7665a5d146050f5e61617632
Attilaerdelyi <145645524+Attilaerdelyi@users.noreply.github.com>
2023-09-25 08:11:29 +0200

    Add files via upload

---
+++ pythonprojekt.py
@@ -0,0 +1,8 @@
+egesz = int(input("adjon meg egy számot"))
+valos = float(input("Adjon meg egy számot"))
+
+print(f" A megadott szám: {egesz}")
+print(f" A megadott szám: {valos}")
+
+print(type(egesz))
+print(type(valos))
